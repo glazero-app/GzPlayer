0dd5be9fe35eaf5a6d2da7407b929a184637bb2d
guoshichao <guoshichao@glazero.com>
2025-04-14 20:45:43 +0800

    录制：修改音视频录制代码

--- source/redplayercore/redrender/video/video_renderer_info.h
+++ source/redplayercore/redrender/video/video_renderer_info.h
@@ -4,13 +4,14 @@
 
 NS_REDRENDER_BEGIN
 
+// 渲染类型
 typedef enum VRClusterType {
-  VRClusterTypeUnknown = 0,
-  VRClusterTypeOpenGL = 1,
-  VRClusterTypeMetal = 2,
-  VRClusterTypeMediaCodec = 3,
-  VRClusterTypeAVSBDL = 5, // AVSampleBufferDisplayLayer
-  VRClusterTypeHarmonyVideoDecoder = 6,
+  VRClusterTypeUnknown = 0,             //< 未知/未指定的渲染类型
+  VRClusterTypeOpenGL = 1,              //< 基于OpenGL/ES的渲染（跨平台通用）
+  VRClusterTypeMetal = 2,               //< 苹果Metal渲染（iOS/macOS专用）
+  VRClusterTypeMediaCodec = 3,          //< Android MediaCodec表面渲染（硬件加速）
+  VRClusterTypeAVSBDL = 5,              //< iOS AVSampleBufferDisplayLayer（硬件解码渲染）
+  VRClusterTypeHarmonyVideoDecoder = 6, //< 鸿蒙系统视频解码渲染
 } VRClusterType;
 
 struct VideoRendererInfo {
